fb492d1491d281ba1a78daa6be784570ffaebb24
anasrasyid <anasrasyid333@gmail.com>
2019-11-03 10:33:37 +0700

    Week 4
    Magnetize

---
+++ Magnetize/Assets/Script/TowerScipt.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerScipt : MonoBehaviour
+{
+    private SpriteRenderer sr;
+    private PlayerController player;
+
+    private void Start()
+    {
+        sr = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    private void OnMouseOver()
+    {
+        sr.color = Color.green;
+        if (player == null)
+            return;
+        if (Input.GetMouseButtonDown(0))
+        {
+            player.StartRotate();
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            player.StopRotate();
+        }
+    }
+
+    private void OnMouseExit()
+    {
+        sr.color = Color.white;
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            player = collision.gameObject.GetComponent<PlayerController>();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            player = null;
+        }
+    }
+}
