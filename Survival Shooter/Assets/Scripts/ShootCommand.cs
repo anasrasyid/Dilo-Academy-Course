5e0501874573ea98ea3628bafed2c98b7afafcdd
anasrasyid <anasrasyid333@gmail.com>
2019-12-24 21:27:38 +0700

    Week 7
    3D Game Survival Shooter Tutorial and
    some design pattern (Factory Pattern and Command Pattern)

---
+++ Survival Shooter/Assets/Scripts/ShootCommand.cs
@@ -0,0 +1,19 @@
+﻿public class ShootCommand : Command
+{
+    PlayerShooting playerShooting;
+
+    public ShootCommand(PlayerShooting playerShooting)
+    {
+        this.playerShooting = playerShooting;
+    }
+
+    public override void Excute()
+    {
+        this.playerShooting.Shoot();
+    }
+
+    public override void UnExecute()
+    {
+
+    }
+}
