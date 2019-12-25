e95012c9b35fb926080fdd55e9ae548fa9d74a3a
anasrasyid <anasrasyid333@gmail.com>
2019-12-25 10:09:32 +0700

    Week 5
    Anggry Birds Looks Like game from Dilo Academy Course

---
+++ AngryBird/Assets/Scripts/Enemy.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class Enemy : MonoBehaviour
+{
+    public float health = 50f;
+    public UnityAction<GameObject> onEnemyDestroyed = delegate { };
+
+    private bool _isHit = false;
+
+    private void OnDestroy()
+    {
+        if (_isHit)
+        {
+            onEnemyDestroyed(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.GetComponent<Rigidbody2D>() == null)
+            return;
+        if(collision.gameObject.tag == "Bird")
+        {
+            _isHit = true;
+            Destroy(gameObject);
+        }else if(collision.gameObject.tag == "Obstacle")
+        {
+            float damage = collision.gameObject.GetComponent<Rigidbody2D>().velocity.magnitude * 10;
+            health -= damage;
+            if(health <= 0)
+            {
+                _isHit = true;
+                Destroy(gameObject);
+            }
+        }
+    }
+}
