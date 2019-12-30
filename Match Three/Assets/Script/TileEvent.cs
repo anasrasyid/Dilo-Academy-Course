0476bca604e49685243fcc2e77b842f434be36a0
anasrasyid <anasrasyid333@gmail.com>
2019-12-30 18:07:28 +0700

    Cleaning

--- Match Three/Assets/Script/TileEvent.cs
+++
@@ -1,9 +0,0 @@
-﻿public abstract class TileEvent
-{
-    //Abstract class untuk base event dari tile
-
-    //Apa yang terjadi jika tile match
-    public abstract void OnMatch();
-    //Check jika persyaratn event telah terpenuhi
-    public abstract bool AchievementCompleted();
-}
